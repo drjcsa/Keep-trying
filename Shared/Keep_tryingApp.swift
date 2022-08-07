e322b959d3444e45b43656f811852b78463600fe
juan carlos <dr.jcsa@outlook.com>
2022-08-06 21:20:53 -0400

    Initial Commit

---
+++ Shared/Keep_tryingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keep_tryingApp.swift
+//  Shared
+//
+//  Created by dr.jcsa on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Keep_tryingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
